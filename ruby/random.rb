ce3186ab1ebdef19924ea214177931e22316a1aa
mm-da-if-kun <9pkjgc76ako96s5sqjxytq46b5xnm7@gmail.com>
2023-11-28 20:43:49 +0900

    表示結果をアルファベット順にします。

--- ruby/random.rb
+++ ruby/random.rb
@@ -7,11 +7,11 @@ def random_group
 
   case group_divide[0]
   when 'half'
-    p group_members[0..2]
-    p group_members[3..5]
+    p group_members[0..2].sort
+    p group_members[3..5].sort
   when 'bias'
-    p group_members[0..1]
-    p group_members[2..5]
+    p group_members[0..1].sort
+    p group_members[2..5].sort
   end
 end
 
