863a610188b6d9556f841dec86a973ac63fde68a
mm-da-if-kun <9pkjgc76ako96s5sqjxytq46b5xnm7@gmail.com>
2023-12-01 20:32:04 +0900

    case文を消去しました。それに伴い配列の添字を変数としランダムな数値としました。

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,18 +1,12 @@
 def random_group
-  # グループ分け用フラグ
-  grouping = ['half', 'bias']
   members = ['A', 'B', 'C', 'D', 'E', 'F']
-  group_divide = grouping.shuffle
   group_members = members.shuffle
 
-  case group_divide[0]
-  when 'half'
-    p group_members[0..2].sort
-    p group_members[3..5].sort
-  when 'bias'
-    p group_members[0..1].sort
-    p group_members[2..5].sort
-  end
+  random = Random.new
+  i = random.rand(1..2)
+
+  p group_members[0..i].sort
+  p group_members[i + 1..5].sort
 end
 
 random_group
