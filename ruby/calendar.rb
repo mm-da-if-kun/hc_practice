9adf18594760b2cd647abce928304e1691a341a4
mm-da-if-kun <9pkjgc76ako96s5sqjxytq46b5xnm7@gmail.com>
2023-12-17 17:36:18 +0900

    calendar.rbを修正します。

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -1,96 +1,95 @@
 require 'optparse'
 require 'date'
 
+# optオブジェクトを作成
 opt = OptionParser.new
 
-begin
-  def to_calendar(cal)
+# Dateオブジェクトを作成
+today = Date.today
+# 今月・今年格納変数
+month = today.mon
+year = today.year
 
-    # Dateオブジェクトを作成
-    today = Date.today
-
-    # オプション・引数がない場合今年・今月を取得する
-    cal[0] = today.mon if cal[0].nil?
-
-    # 日付取得変数
-    input_day = cal[0].to_i
-
-    # 月初日付・月末日付を取得
-    first_day = Date.new(today.year, input_day)
-    last_day = Date.new(today.year, input_day, -1)
-
-    # 出力日付格納配列
-    month_day = [*first_day.mday..last_day.mday]
-
-    # 曜日の数値格納変数
-    day_of_week_count = first_day.wday
-
-    # 日付格納用変数
-    week_day = []
-
-    # 曜日格納用配列
-    day_of_week = ['月', '火', '水', '木', '金', '土', '日']
-
-    # 月・年出力用変数
-    cal_month_year = ("#{cal[0]}月" + ' ' + today.year.to_s)
-
-    # 出力用文字列格納配列
-    output_display = []
-    output_display[0] = day_of_week
-    
-    # 型変換用配列の添字カウンタ
-    i_count = 0
-
-    # 出力配列の添字カウンタ
-    out_count = 0
-
-    # 月の桁数により表示位置の調整
-    case input_day
-    when 1..9
-      puts cal_month_year.rjust(13)
-    when 10..12
-      puts cal_month_year.rjust(14)
-    end
-    
-    # 日付を配列に格納する
-    month_day.each do |n|
-      # 日付を対応する曜日の数値に格納する
-      week_day[day_of_week_count - 1] = n
-      # 配列の中身がnilの場合に半角スペースを代入する
-      week_day[i_count] = ' ' if week_day[i_count].nil?
-      # String型に変換して格納する値を2桁にする
-      day_array = week_day[i_count].to_s
-      week_day[i_count] = day_array.rjust(2, ' ')
-      # それぞれの配列の添字をカウント
-      day_of_week_count += 1
-      i_count += 1
-    end
-
-    # 配列を1週間ごとに分割
-    week_day = week_day.each_slice(7).to_a
-    # 半角スーペースで結合して1週間ごとに出力する
-    while out_count < week_day.length + 1
-      output_display[out_count + 1] = week_day[out_count]
-      puts output_display[out_count].join(' ')
-      out_count += 1
-    end
-
-  end
-  
-  # optparseオブジェクトに-mオプションを登録する
-  opt.on('-m'){
-    to_calendar(ARGV)
-  }
-  
-  # オプション・引数を指定しない場合のメソッド呼び出し
-  if ARGV == []
-    to_calendar(ARGV)
-  end
-  
-  # parseすることで引数だけを取り出す
-  opt.parse!(ARGV)
-  
-# -mの引数が不正な月の場合は下記の文言を出力
-rescue => exception
-  puts "22 is neither a month number (1..12) nor a name"
+# オプション・引数を指定した場合にその月を取得
+unless ARGV[1].nil?
+  month = ARGV[1].to_i
 end
+
+# -mの引数が不正な月の場合(1~12以外の場合)エラーを出力する
+unless (1..12).cover?(month)
+  raise "#{month} is neither a month number (1..12) nor a name"
+end
+
+# -mオプションを登録する
+opt.on('-m')
+
+# parseすることで引数だけを取り出す
+opt.parse!(ARGV)
+
+# カレンダー出力用メソッド
+def to_calendar(year, month)
+  # 月初日付・月末日付を取得
+  first_day = Date.new(year, month)
+  last_day = Date.new(year, month, -1)
+
+  # 出力日付格納配列
+  month_day = [*first_day.mday..last_day.mday]
+
+  # 曜日の数値格納変数
+  day_of_week_count = 0
+
+  # 曜日の数値格納変数(月曜始まりなため「-1」する)
+  # 月初の日付が日曜日始まり(wdayが0の場合配列の位置を調整するために6を代入)
+  # それ以外は月初の日付から「-1」した位置から日付を格納していく
+  if first_day.wday.zero?
+    day_of_week_count = 6
+  else
+    day_of_week_count = first_day.wday - 1
+  end
+
+  # 月・年出力
+  puts ("#{month}月 #{year}").center(20)
+
+  # 曜日出力
+  puts '月 火 水 木 金 土 日'
+
+  # 出力用文字列格納配列
+  output_display = []
+
+  # 出力配列の添字カウンタ
+  out_count = 0
+
+  # 日付を格納するまでの位置を半角スペースで埋める
+  month_day.each do |day|
+    if output_display == []
+      while out_count < day_of_week_count
+        output_display[out_count] = ' '
+        printf("%2s\s", output_display[out_count])
+        out_count += 1
+      end
+    end
+
+    # 続きの位置から日付を格納する
+    output_display[day_of_week_count] = day
+    
+    # 日付出力
+    printf("%2s\s", output_display[day_of_week_count])
+
+    # 1週間分の日付を格納後、改行する
+    # 配列格納位置を0でリセット
+    if day_of_week_count == 6
+      puts "\n"
+      day_of_week_count = 0
+    elsif day_of_week_count < 6
+      day_of_week_count += 1
+    end
+  end
+
+  # 最後に出力されてしまう「%」を出力させないようにする
+  unless day_of_week_count.zero?
+    puts ""
+  end
+end
+
+# カレンダー出力メソッド呼び出し
+to_calendar(year, month)
